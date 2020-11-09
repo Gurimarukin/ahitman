611a75c978f36d9d0d79567633083c697d245e2e
Gurimarukin <naxel@laposte.net>
2020-11-09 20:52:48 +0100

    Update layout

--- src/App.svelte
+++ src/App.svelte
@@ -5,14 +5,53 @@
   import Link from './ui/Link.svelte'
 </script>
 
+<style>
+  main {
+    font-size: 1.2em;
+  }
+
+  h1 {
+    margin-bottom: 0;
+  }
+
+  p.subtitle {
+    margin-top: 0.33em;
+    font-style: italic;
+  }
+
+  blockquote {
+    margin-left: 10px;
+    padding-left: 30px;
+    border-left: 4px solid lightgrey;
+    font-style: italic;
+    font-size: 0.9em;
+  }
+
+  li {
+    font-size: 1.17em;
+    font-weight: bold;
+  }
+</style>
+
 <main>
   <h1>Odolf Hitman</h1>
-  <p>Assassin humaniste.</p>
+  <p class="subtitle">Assassin humaniste.</p>
 
   <hr />
 
-  <h3>Odolf Hitman: Genesis</h3>
-  <Link to={routes.ohGenesis}>lire...</Link>
+  <blockquote>LA SERI EVENMENT</blockquote>
+
+  <blockquote>"Le héros de l'année" - jeu video point com</blockquote>
+
+  <blockquote>Grand gagnant du prix Eric Zeymour tout ca tout ca</blockquote>
+
+  <hr />
+
+  <ul>
+    <li>
+      <Link to={routes.ohGenesis}>Odolf Hitman: Genesis</Link>
+    </li>
+  </ul>
 
   <Router />
 </main>

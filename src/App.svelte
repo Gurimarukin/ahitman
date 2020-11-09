94591b2be1da32051fb23b9605beb368ae1ed263
Gurimarukin <naxel@laposte.net>
2020-11-09 09:33:16 +0100

    Layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
+  // import { images } from './images'
 </script>
 
 <style>
+  /* .images {
+    margin-bottom: 24px;
+  } */
+
+  /* img {
+    display: block;
+    max-height: 500px;
+    max-width: 500px;
+    border: 1px solid black;
+  } */
+
+  /* img + img {
+    margin-top: 12px;
+  } */
 </style>
 
 <main>
   <h1>Odolf Hitman</h1>
   <p>Assassin humaniste.</p>
+
+  <hr />
+
+  <h2>Odolf Hitman: Genesis</h2>
+  <a href="#oh-genesis">lire...</a>
+  <!-- <div class="images">
+    {#each images.ohGenesis as image (image)}<img alt={image} src={image} />{/each}
+  </div> -->
 </main>

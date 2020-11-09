0c0fd19798cd05a78e334dae97fb34d8d3bbdbeb
Gurimarukin <naxel@laposte.net>
2020-11-09 20:57:14 +0100

    Extract title

--- src/Router.svelte
+++ src/Router.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
+  import type { Comic } from './images'
   import { images } from './images'
   import { routes } from './routes'
   import { historyStore } from './stores/historyStore'
 
   import Player from './Player.svelte'
 
-  $: comic = $historyStore.location.hash === routes.ohGenesis ? images.ohGenesis : null
+  $: comic = ((): [string, Comic] | null => {
+    const { hash } = $historyStore.location
+
+    if (hash === routes.ohGenesis) return images.ohGenesis
+
+    return null
+  })()
 </script>
 
 {#if comic !== null}
-  <Player {comic} />
+  <Player comic={comic[1]} />
 {/if}
--- src/images.ts
+++ src/images.ts
@@ -1,16 +1,19 @@
 export type Comic = string[]
 
-const ohGenesis: Comic = [
-  'img/oh-genesis/1.jpg',
-  'img/oh-genesis/2.jpg',
-  'img/oh-genesis/3.jpg',
-  'img/oh-genesis/4.jpg',
-  'img/oh-genesis/5.jpg',
-  'img/oh-genesis/6.jpg',
-  'img/oh-genesis/7.jpg',
-  'img/oh-genesis/8.jpg',
-  'img/oh-genesis/9.jpg',
-  'img/oh-genesis/10.jpg'
+const ohGenesis: [string, Comic] = [
+  'Odolf Hitman: Genesis',
+  [
+    'img/oh-genesis/1.jpg',
+    'img/oh-genesis/2.jpg',
+    'img/oh-genesis/3.jpg',
+    'img/oh-genesis/4.jpg',
+    'img/oh-genesis/5.jpg',
+    'img/oh-genesis/6.jpg',
+    'img/oh-genesis/7.jpg',
+    'img/oh-genesis/8.jpg',
+    'img/oh-genesis/9.jpg',
+    'img/oh-genesis/10.jpg'
+  ]
 ]
 
 export const images = { ohGenesis }
